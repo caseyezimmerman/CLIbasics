7ae99b034de277ef01064e410b176576bc5c3585
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-09-06 12:49:41 -0400

    notes

---
+++ newPythonFile.py
@@ -0,0 +1,61 @@
+print "Hello, World"
+print("Hello, World")
+# this is a comment
+print """
+this
+is multiple
+line """
+
+name = "Casey Zimmerman"; #this is a string
+
+#data types: Strings, numbers (floats, integers), boolean, dictionary, list, objects)
+
+name = "Casey" + "Zimmerman" # + is a concatenate symbol with strings
+first = "casey"
+last = "zimmerman"
+full_name = first + " " + last;
+print full_name
+meaning_of_life = 42 + 2; # + is now an addition symbol
+forty_two = 84 / 2;
+forty_two = 21 * 2;
+
+#python cannot add a string to an integer
+
+print "-" * 20;
+
+first_name = "casey"
+last_name = "zimmerman"
+#how to intermigle english and vars
+print "Hello {} {}".format(first_name, last_name) 
+
+print "Hello %s" % "Casey"
+
+#floats
+pi = 3.14
+print type(pi)
+
+#make a float an integer
+makeMeAnInt = int(pi);
+print makeMeAnInt
+print type(makeMeAnInt)
+
+#booleans
+the_truth = True;
+print type(the_truth)
+
+addOneToMe = 1
+#addOneToMe = addOneToMe + 1
+#cannot do addOneToMe++
+addOneToMe += 1
+print addOneToMe
+
+#order of operations
+#use ()
+
+name = raw_input("What is your name?")
+
+
+
+
+
+
